573d93672f7ee5a5d000de1c366e856e2d075b67
Chad Nedzlek <chadnedz@microsoft.com>
2020-08-06 12:21:00 -0700

    Different menu on Windows/Mac

--- App/Views/MainWindow.xaml.cs
+++ App/Views/MainWindow.xaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.InteropServices;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Primordially.Core;
@@ -6,8 +8,11 @@ namespace Primordially.App.Views
 {
     public class MainWindow : Window
     {
+        public bool IsWindows { get; }
+
         public MainWindow()
         {
+            IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
             InitializeComponent();
         }
 
