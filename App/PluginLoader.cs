a81fd0bf96515ecc2170605abc9355c3cb9c8e01
Alex Perovich <alperovi@microsoft.com>
2020-08-05 23:38:21 -0700

    Fix casing

--- App/PluginLoader.cs
+++ App/PluginLoader.cs
@@ -7,7 +7,7 @@ namespace PCSharpGen.App
 {
     internal class PluginLoader
     {
-        private static readonly string BasePath = Path.Join(AppContext.BaseDirectory, "Plugins");
+        private static readonly string BasePath = Path.Join(AppContext.BaseDirectory, "plugins");
         private static readonly Dictionary<string, Assembly> LoadedPlugins = new Dictionary<string, Assembly>();
 
         public static IEnumerable<string> ListAvailable()
